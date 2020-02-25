8d759c5d893524901461570e115b34ab8e59da25
littlesarker <61264159+littlesarker@users.noreply.github.com>
2020-02-25 22:59:39 +0600

    left hand triangle

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- main()
+int main()
 {
      int i,j;
 
      for(i=1;i<=5;i++)
      {
-        for(j=1;j<=5;j++)
-        {
-        printf("* ");
-        }
-
+       for(j=1;j<=i;j++)
+       {
+       printf("* ");
+       }
      printf("\n");
      }
 
